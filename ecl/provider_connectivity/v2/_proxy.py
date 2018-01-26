cc53bd33c8a6a5ffd70eb81f3230e5768592a54b
htysh <h.ohta@ntt.com>
2018-01-26 16:42:02 +0900

    :sparkles: IF-438 added gcp connection methods

--- ecl/provider_connectivity/v2/_proxy.py
+++ ecl/provider_connectivity/v2/_proxy.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ecl.provider_connectivity.v2 import aws_connection as _aws_connection
+from ecl.provider_connectivity.v2 import gcp_connection as _gcp_connection
 from ecl.provider_connectivity.v2 import exchange_point as _exchange_point
 from ecl.provider_connectivity.v2 import operation as _operation
 from ecl import proxy2
@@ -145,3 +146,104 @@ class Proxy(proxy2.BaseProxy):
         return list(self._list(_operation.Operation,
                                paginated=False,
                                **params))
+
+    def gcp_connections(self, **params):
+        """List connection resource between ECL2.0 and GCP.
+
+        :param params: The parameters as query string
+            to get connections by specified condition.
+        :returns: A list of connection objects
+        :rtype: list of :class:
+                `~ecl.provider_connectivity.v1.gcp_connection.GCPConnection`
+        """
+        return list(self._list(_gcp_connection.GCPConnection,
+                               paginated=False,
+                               **params))
+
+    def get_gcp_connection(self, connection_id):
+        """Show connection resource between ECL2.0 and GCP.
+
+        :param string connection_id: ID of specified connection.
+        :return: :class:`
+                ~ecl.provider_connectivity.v1.gcp_connection.GCPConnection`
+        """
+        return self._get(_gcp_connection.GCPConnection, connection_id)
+
+    def find_gcp_connection(self, name_or_id, ignore_missing=False):
+        """Find a single connection.
+
+        :param name_or_id: The name or ID of a connection.
+        :param bool ignore_missing: When set to ``False``
+                    :class:`~ecl.exceptions.ResourceNotFound` will be
+                    raised when the resource does not exist.
+                    When set to ``True``, None will be returned when
+                    attempting to find a nonexistent resource.
+        :returns: One :class:
+                `~ecl.network.v2.connection.GCPConnection` or None
+        """
+        return self._find(_gcp_connection.GCPConnection,
+                          name_or_id,
+                          ignore_missing=ignore_missing)
+
+    def create_gcp_connection(self, pairing_key_primary, pairing_key_secondary,
+                              bandwidth, qos_type, exchange_point_id,
+                              name=None, description=None):
+        """Create connection between ECL2.0 and GCP.
+
+        :param string pairing_key_primary: Pairing Key
+            for Active Cloud Router in GCP.
+        :param string pairing_key_secondary: Pairing Key
+            for Standby Cloud Router in GCP
+        :param string bandwidth: Bandwidth(Mbps). When you select Guarantee,
+            choose 50,100,200,300,400,500,1000.
+        :param string qos_type: QoS type of network.
+            You can choose Guarantee.
+        :param string exchange_point_id: ID of exchange_points.
+        :param string name: Name of connection.
+        :param string description: Description of connection.
+        """
+        body = {}
+        body["pairing_key_primary"] = pairing_key_primary
+        body["pairing_key_secondary"] = pairing_key_secondary
+        body["bandwidth"] = bandwidth
+        body["qos_type"] = qos_type
+        body["exchange_point_id"] = exchange_point_id
+        if name:
+            body["name"] = name
+        if description:
+            body["description"] = description
+        return self._create(_gcp_connection.GCPConnection, **body)
+
+    def update_gcp_connection(self, connection, **params):
+        """Update connection between ECL2.0 and GCP.
+
+        :param string connection_id: The ID of a connection.
+        :attrs \*\*params: Parameters for connection update.
+
+            * string name: Name of connection.
+            * string description: Description of connection.
+            * string bandwidth: Bandwidth(Mbps). When you select Guarantee,
+                choose 50,100,200,300,400,500,1000.
+        """
+        if not isinstance(connection, _gcp_connection.GCPConnection):
+            connection = self._get_resource(_gcp_connection.GCPConnection,
+                                            connection)
+            connection._body.clean()
+        return self._update(_gcp_connection.GCPConnection,
+                            connection, **params)
+
+    def delete_gcp_connection(self, connection_id, ignore_missing=False):
+        """Delete connection between ECL2.0 and GCP.
+
+        :param connection_id: The ID of a connection.
+        :param bool ignore_missing: When set to ``False``
+                    :class:`~ecl.exceptions.ResourceNotFound` will be
+                    raised when the port does not exist.
+                    When set to ``True``, no exception will be set when
+                    attempting to delete a nonexistent port.
+
+        :returns: ``None``
+        """
+        self._delete(_gcp_connection.GCPConnection,
+                     connection_id,
+                     ignore_missing=ignore_missing)
---
+++ ecl/provider_connectivity/v2/gcp_connection.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+from ecl.provider_connectivity import provider_connectivity_service
+from ecl import resource2
+
+
+class GCPConnection(resource2.Resource):
+    resources_key = "gcp_connections"
+    resource_key = "gcp_connection"
+    service = provider_connectivity_service.ProviderConnectivityService("v2.0")
+    base_path = '/' + service.version + '/gcp_connections'
+
+    # Capabilities
+    allow_list = True
+    allow_get = True
+    allow_create = True
+    allow_update = True
+    allow_delete = True
+
+    # _query_mapping = resource2.QueryParameters()
+    # TBD
+
+    # Properties
+    #: It identifies connection resource uniquely.
+    id = resource2.Body('id')
+    #: Name of connection.
+    name = resource2.Body('name')
+    #: Description of connection.
+    description = resource2.Body('description')
+    #: Status of connection.
+    status = resource2.Body('status')
+    #: Status of operation for gcp_connection.
+    operation_status = resource2.Body('operation_status')
+    #: Pairing Key for Active Cloud Router in GCP.
+    pairing_key_primary = resource2.Body('pairing_key_primary')
+    #: Pairing Key for Standby Cloud Router in GCP.
+    pairing_key_secondary = resource2.Body('pairing_key_secondary')
+    #: Tenant ID of the owner.
+    tenant_id = resource2.Body('tenant_id')
+    #: Bandwidth assigned with this connection.
+    bandwidth = resource2.Body('bandwidth')
+    #: QoS type assigned with this connection.
+    qos_type = resource2.Body('qos_type')
+    #: ID of exchange_points.
+    exchange_point_id = resource2.Body('exchange_point_id')
+    #: VLAN ID.
+    vlan = resource2.Body('vlan')
+    #: ID of gcp_gateway which gcp_connection connected.
+    gcp_gateway_id = resource2.Body('gcp_gateway_id')
+    #: ID of gcp_interface which gcp_connection connected.
+    gcp_interface_id = resource2.Body('gcp_interface_id')
+    #: Name of Region where customer's cloud router exist.
+    gcp_region_name = resource2.Body('gcp_region_name')
+    #: Name of primary Interconnect.
+    primary_interconnect_name = resource2.Body('primary_interconnect_name')
+    #: Name of primary Interconnect Attachment.
+    primary_interconnect_attachment_name = \
+        resource2.Body('primary_interconnect_attachment_name')
+    #: Name of secondary Interconnect.
+    secondary_interconnect_name = resource2.Body('secondary_interconnect_name')
+    #: Name of secondary Interconnect Attachment.
+    secondary_interconnect_attachment_name = \
+        resource2.Body('secondary_interconnect_attachment_name')
