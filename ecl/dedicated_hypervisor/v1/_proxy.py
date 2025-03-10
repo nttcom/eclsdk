6eed396e53fee68dbf156b6f6152336ac0a8a56b
柏原由己 <yoshikikashiwabara@A1015168.local>
2025-03-10 17:53:08 +0900

    :sparkles: IF-14188 add vCenter API

--- ecl/dedicated_hypervisor/v1/_proxy.py
+++ ecl/dedicated_hypervisor/v1/_proxy.py
@@ -271,6 +271,7 @@ class Proxy(proxy2.BaseProxy):
         Shows the registered vCenter server information.
 
         :return: :class:`~ecl.dedicated_hypervisor.v1.vcenter.VCenter`
+        instance.
         """
         return list(self._list(_vcenter.VCenter))
 
