1c6ffca8b4ea841694316e4d870265052acb6389
sugimo-s <91592177+sugimo-s@users.noreply.github.com>
2025-03-19 07:51:53 +0900

    Revert "Kashiwabara if 14188 v center addition (#161)" (#163)
    This reverts commit 27bc19046aa17b792f879156deaa8faf1f28c56e.

--- ecl/dedicated_hypervisor/exceptions.py
+++ ecl/dedicated_hypervisor/exceptions.py
@@ -6,21 +6,7 @@ from ecl import exceptions
 
 
 class HttpException(exceptions.HttpException):
-
-    def _get_exception_message(self, message=None):
-        try:
-            content = json.loads(self.response._content.decode('utf-8'))
-
-            # vCenter error case.
-            key = list(content.keys())[0]
-            code = content[key]['code']
-            message = content[key]['message']
-            return '[' + str(code) + ']' + message
-
-        except:
-            pass
-
-        return super()._get_exception_message(message=message)
+    pass
 
 
 class NotFoundException(HttpException):
