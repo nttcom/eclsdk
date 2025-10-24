78ec9966325627041b79c82ec8479c02bc80d5e9
t-yokoix <takahide.x.yokoi@ntt.com>
2025-10-24 16:21:16 +0900

    :sparkles: IF-16696 Block storage Gen2: Add parameter to Volume API

--- ecl/storage/v1/_proxy.py
+++ ecl/storage/v1/_proxy.py
@@ -222,6 +222,7 @@ class Proxy(proxy2.BaseProxy):
         :param throughput: Provisioned throughput for volume.
         :param initiator_iqns: List of initiator IQN who can access to this volume.
         :param availability_zone: Availability zone.
+        :param snapshot_reserve_size: Size of Snapshot Reserve
         :return: :class:`~ecl.storage.v1.volume.Volume`
         """
         body = {"virtual_storage_id": virtual_storage_id, "name": name, "size": size}
@@ -235,6 +236,8 @@ class Proxy(proxy2.BaseProxy):
             body["initiator_iqns"] = initiator_iqns
         if availability_zone:
             body["availability_zone"] = availability_zone
+        if snapshot_reserve_size:
+            body["snapshot_reserve_size"] = snapshot_reserve_size
         volume = _volume.Volume()
         return volume.create(self.session, **body)
 
--- ecl/storage/v1/volume.py
+++ ecl/storage/v1/volume.py
@@ -73,6 +73,15 @@ class Volume(resource2.Resource):
     #: Percentage of Used Snapshots
     percentage_snapshot_reserve_used = \
         resource2.Body('percentage_snapshot_reserve_used', type=int)
+    #: Percentage of Snapshots Reserve
+    percentage_snapshot_reserve = \
+        resource2.Body('percentage_snapshot_reserve', type=int)
+    #: The size of Snapshots Reserve
+    snapshot_reserve_size = \
+        resource2.Body('snapshot_reserve_size', type=int)
+    #: The size of Used Snapshots Reserve
+    snapshot_reserve_used = \
+        resource2.Body('snapshot_reserve_used', type=int)
     #: properties for smb storage
     smb_properties = resource2.Body('smb_properties', type=dict)
 
