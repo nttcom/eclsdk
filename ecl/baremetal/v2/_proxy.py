52fb6e5d329101eeae779a2c8baa5655cdec13d6
MikuInoueTx <miku.inoue.tc@ntt-tx.co.jp>
2026-02-20 17:16:13 +0900

    :sparkles: IF-17146 Support Update BMC Password (#209)

--- ecl/baremetal/v2/_proxy.py
+++ ecl/baremetal/v2/_proxy.py
@@ -467,6 +467,18 @@ class Proxy(proxy2.BaseProxy):
         server = _server.ServerAction()
         return server.reset_bmc(self.session, server_id, type)
 
+    def update_bmc_password(self, server_id, password):
+        """Update the password for the Baseboard Management Controller
+        of the Baremetal Server associated with server_id.
+        This request will be accepted only when the task_state is None.
+
+        :param string server_id: ID for the server.
+        :param string password: New password for BMC.
+        :return: ``None``
+        """
+        server = _server.ServerAction()
+        return server.update_bmc_password(self.session, server_id, password)
+
     def metadata(self, server_id):
         """This API lists metadata for a specified server.
 
