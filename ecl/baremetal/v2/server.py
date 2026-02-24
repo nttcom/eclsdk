6ad62b9b2c3200d225d7bc95432d2eb143f6a31b
t-yokoix <takahide.x.yokoi@ntt.com>
2026-02-24 09:52:46 +0900

    :bug: Remove unnecessary code

--- ecl/baremetal/v2/server.py
+++ ecl/baremetal/v2/server.py
@@ -384,6 +384,6 @@ class ServerActionRemoteConsole(resource2.Resource):
             endpoint_filter=self.service,
             json=body
         )
-        self._translate_response(resp, has_body=True)
+        self._translate_response(resp, has_body=False)
         return self
 
